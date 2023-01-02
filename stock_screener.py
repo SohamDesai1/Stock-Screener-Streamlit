0df000c7c055289a473ac61fef27086fddd175c2
SohamDesai1 <sohamdesai123@gmail.com>
2023-01-03 00:10:51 +0530

    different intervals for each time period

--- stock_screener.py
+++ stock_screener.py
@@ -59,8 +59,18 @@ with stocks:
         st.error("Error: End date must fall after start date.")
     elif goto_today:
         end_date = today_date
+        diff = end_date - start_date
+        if diff.days <= 365 or 180 > diff.days < 365:
+            interval = "1d"
+        elif 90 >= diff.days >= 30:
+            interval = "90m"
+        elif 365 < diff.days > 730:
+            interval = "1wk"
+        else:
+            interval = "1h"
+        st.write(interval)
         df_stock = yf.download(f"{stock}.NS", start=start_date,
-                         end=end_date, interval="15m")
+                               end=end_date, interval=interval)
         df_stock.style.set_properties(**{'text-align': 'left'})
         st.write(df_stock)
         fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.03, row_heights=[
@@ -69,19 +79,31 @@ with stocks:
         fig.update_xaxes(
             rangebreaks=[
                 dict(bounds=["sat", "mon"]),
-                dict(pattern='hour', bounds=[16, 9])
+                # dict(pattern='hour', bounds=[16, 9])
             ]
         )
 
         fig.add_trace(go.Candlestick(x=df_stock.index, open=df_stock['Open'], high=df_stock['High'],
-                      low=df_stock['Low'], close=df_stock['Close'], name='market data'), row=1, col=1)
+                                     low=df_stock['Low'], close=df_stock['Close'], name='market data'), row=1, col=1)
+        # bar chart
         fig.add_trace(
             go.Bar(x=df_stock.index, y=df_stock['Volume'], name='Volume'), row=2, col=1)
 
         st.plotly_chart(fig, use_container_width=True)
     elif get_stock:
+        # get the difference between the two dates
+        diff = end_date - start_date
+        if diff.days <= 365 or 180 > diff.days < 365:
+            interval = "1d"
+        elif 90 >= diff.days >= 30:
+            interval = "90m"
+        elif 365 < diff.days > 730:
+            interval = "1wk"
+        else:
+            interval = "1h"
+        st.write(interval)
         df_get_stock = yf.download(f"{stock}.NS", start=start_date,
-                         end=end_date+datetime.timedelta(days=1), interval="15m")
+                                   end=end_date+datetime.timedelta(days=1), interval=interval)
         df_get_stock.style.set_properties(**{'text-align': 'left'})
         st.write(df_get_stock)
         fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.03, row_heights=[
@@ -90,7 +112,7 @@ with stocks:
         fig.update_xaxes(
             rangebreaks=[
                 dict(bounds=["sat", "mon"]),
-                dict(pattern='hour', bounds=[16, 9])
+                # dict(pattern='hour', bounds=[16, 9])
             ]
         )
 
@@ -105,31 +127,32 @@ with stocks:
 
 with indicators:
     # select = st.sidebar.selectbox("Select a indicator",["aberration", "above", "above_value", "accbands", "ad", "adosc", "adx", "alma", "amat", "ao", "aobv", "apo", "aroon", "atr", "bbands","below", "below_value", "bias", "bop", "brar", "cci," "cdl_pattern", "cdl_z", "cfo", "cg", "chop","cksp", "cmf", "cmo", "coppock", "cross", "cross_value", "cti," "decay", "decreasing", "dema", "dm", "donchian", "dpo", "ebsw", "efi", "ema", "entropy", "eom", "er ","eri", "fisher", "fwma", "ha", "hilo", "hl2," "hlc3", "hma," "hwc," "hwma", "ichimoku", "increasing", "inertia", "jma", "kama", "kc", "kdj," "kst," "kurtosis", "kvo," "linreg", "log_return", "long_run," "macd"," mad", "massi", "mcgd", "median", "mfi," "midpoint", "midprice", "mom," "natr", "nvi," "obv," "ohlc4", "pdist", "percent_return", "pgo," "ppo," "psar", "psl," "pvi," "pvo," "pvol", "pvr," "pvt," "pwma", "qqe," "qstick", "quantile", "rma," "roc," "rsi," "rsx," "rvgi", "rvi," "short_run", "sinwma", "skew", "slope", "sma", "smi", "squeeze", "squeeze_pro", "ssf," "stc," "stdev", "stoch", "stochrsi", "supertrend", "swma", "t3, ""td_seq", "tema", "thermo", "tos_stdevall", "trima", "trix", "true_range", "tsi," "tsignals", "ttm_trend", "ui, ""uo, ""variance", "vhf," "vidya", "vortex", "vp, ""vwap", "vwma", "wcp," "willr", "wma," "xsignals", "zlma", "zscore"])
-    candles = "2crows","3blackcrows","3inside","3linestrike","3outside","3starsinsouth","3whitesoldiers","abandonedbaby","advanceblock","belthold","breakaway","closingmarubozu","concealbabyswall","counterattack","darkcloudcover","doji","dojistar","dragonflydoji","engulfing","eveningdojistar","eveningstar","gapsidesidewhite","gravestonedoji","hammer","hangingman","harami","haramicross","highwave","hikkake","hikkakemod","homingpigeon","identical3crows","inneck","inside","invertedhammer","kicking","kickingbylength","ladderbottom","longleggeddoji","longline","marubozu","matchinglow","mathold","morningdojistar","morningstar","onneck","piercing","rickshawman","risefall3methods","separatinglines","shootingstar","shortline","spinningtop","stalledpattern","sticksandwich","takuri","tasukigap","thrusting","tristar","unique3river","upsidegap2crows","xsidegap3methods","Heikin-Ashi: ha","Z Score: cdl_z"
+    candles = "2crows", "3blackcrows", "3inside", "3linestrike", "3outside", "3starsinsouth", "3whitesoldiers", "abandonedbaby", "advanceblock", "belthold", "breakaway", "closingmarubozu", "concealbabyswall", "counterattack", "darkcloudcover", "doji", "dojistar", "dragonflydoji", "engulfing", "eveningdojistar", "eveningstar", "gapsidesidewhite", "gravestonedoji", "hammer", "hangingman", "harami", "haramicross", "highwave", "hikkake", "hikkakemod", "homingpigeon", "identical3crows", "inneck", "inside", "invertedhammer", "kicking", "kickingbylength", "ladderbottom", "longleggeddoji", "longline", "marubozu", "matchinglow", "mathold", "morningdojistar", "morningstar", "onneck", "piercing", "rickshawman", "risefall3methods", "separatinglines", "shootingstar", "shortline", "spinningtop", "stalledpattern", "sticksandwich", "takuri", "tasukigap", "thrusting", "tristar", "unique3river", "upsidegap2crows", "xsidegap3methods", "Heikin-Ashi: ha", "Z Score: cdl_z"
 
     cycles = "Even Better Sinewave: ebsw"
 
-    momentum = "Awesome Oscillator: ao","Absolute Price Oscillator: apo","Bias: bias","Balance of Power: bop","BRAR: brar","Commodity Channel Index: cci","Chande Forecast Oscillator: cfo","Center of Gravity: cg","Chande Momentum Oscillator: cmo","Coppock Curve: coppock","Correlation Trend Indicator: cti","Directional Movement: dm","Efficiency Ratio: er","Elder Ray Index: eri","Fisher Transform: fisher","Inertia: inertia","KDJ: kdj","KST Oscillator: kst","Moving Average Convergence Divergence: macd","Momentum: mom","Pretty Good Oscillator: pgo","Percentage Price Oscillator: ppo","Psychological Line: psl","Percentage Volume Oscillator: pvo","Quantitative Qualitative Estimation: qqe","Rate of Change: roc","Relative Strength Index: rsi","Relative Strength Xtra: rsx","Relative Vigor Index: rvgi","Schaff Trend Cycle: stc","Slope: slope","SMI Ergodic smi","Squeeze: squeeze","Squeeze Pro: squeeze_pro","Stochastic Oscillator: stoch","Stochastic RSI: stochrsi","TD Sequential: td_seq","Trix: trix","True strength index: tsi","Ultimate Oscillator: uo","Williams %R: willr"
+    momentum = "Awesome Oscillator: ao", "Absolute Price Oscillator: apo", "Bias: bias", "Balance of Power: bop", "BRAR: brar", "Commodity Channel Index: cci", "Chande Forecast Oscillator: cfo", "Center of Gravity: cg", "Chande Momentum Oscillator: cmo", "Coppock Curve: coppock", "Correlation Trend Indicator: cti", "Directional Movement: dm", "Efficiency Ratio: er", "Elder Ray Index: eri", "Fisher Transform: fisher", "Inertia: inertia", "KDJ: kdj", "KST Oscillator: kst", "Moving Average Convergence Divergence: macd", "Momentum: mom", "Pretty Good Oscillator: pgo", "Percentage Price Oscillator: ppo", "Psychological Line: psl", "Percentage Volume Oscillator: pvo", "Quantitative Qualitative Estimation: qqe", "Rate of Change: roc", "Relative Strength Index: rsi", "Relative Strength Xtra: rsx", "Relative Vigor Index: rvgi", "Schaff Trend Cycle: stc", "Slope: slope", "SMI Ergodic smi", "Squeeze: squeeze", "Squeeze Pro: squeeze_pro", "Stochastic Oscillator: stoch", "Stochastic RSI: stochrsi", "TD Sequential: td_seq", "Trix: trix", "True strength index: tsi", "Ultimate Oscillator: uo", "Williams %R: willr"
 
-    overlap = "Arnaud Legoux Moving Average: alma","Double Exponential Moving Average: dema","Exponential Moving Average: ema","Fibonacci's Weighted Moving Average: fwma","Gann High-Low Activator: hilo","High-Low Average: hl2","High-Low-Close Average: hlc3","Commonly known as 'Typical Price' in Technical Analysis literature","Hull Exponential Moving Average: hma","Holt-Winter Moving Average: hwma","Ichimoku Kinkō Hyō: ichimoku","Jurik Moving Average: jma","Kaufman's Adaptive Moving Average: kama","Linear Regression: linreg","McGinley Dynamic: mcgd","Midpoint: midpoint","Midprice: midprice","Open-High-Low-Close Average: ohlc4","Pascal's Weighted Moving Average: pwma","WildeR's Moving Average: rma","Sine Weighted Moving Average: sinwma","Simple Moving Average: sma","Ehler's Super Smoother Filter: ssf","Supertrend: supertrend","Symmetric Weighted Moving Average: swma","T3 Moving Average: t3","Triple Exponential Moving Average: tema","Triangular Moving Average: trima","Variable Index Dynamic Average: vidya","Volume Weighted Average Price: vwap","Requires the DataFrame index to be a DatetimeIndex","Volume Weighted Moving Average: vwma","Weighted Closing Price: wcp","Weighted Moving Average: wma","Zero Lag Moving Average: zlma"
+    overlap = "Arnaud Legoux Moving Average: alma", "Double Exponential Moving Average: dema", "Exponential Moving Average: ema", "Fibonacci's Weighted Moving Average: fwma", "Gann High-Low Activator: hilo", "High-Low Average: hl2", "High-Low-Close Average: hlc3", "Commonly known as 'Typical Price' in Technical Analysis literature", "Hull Exponential Moving Average: hma", "Holt-Winter Moving Average: hwma", "Ichimoku Kinkō Hyō: ichimoku", "Jurik Moving Average: jma", "Kaufman's Adaptive Moving Average: kama", "Linear Regression: linreg", "McGinley Dynamic: mcgd", "Midpoint: midpoint", "Midprice: midprice", "Open-High-Low-Close Average: ohlc4", "Pascal's Weighted Moving Average: pwma", "WildeR's Moving Average: rma", "Sine Weighted Moving Average: sinwma", "Simple Moving Average: sma", "Ehler's Super Smoother Filter: ssf", "Supertrend: supertrend", "Symmetric Weighted Moving Average: swma", "T3 Moving Average: t3", "Triple Exponential Moving Average: tema", "Triangular Moving Average: trima", "Variable Index Dynamic Average: vidya", "Volume Weighted Average Price: vwap", "Requires the DataFrame index to be a DatetimeIndex", "Volume Weighted Moving Average: vwma", "Weighted Closing Price: wcp", "Weighted Moving Average: wma", "Zero Lag Moving Average: zlma"
 
-    performance = "Draw Down: drawdown","Log Return: log_return","Percent Return: percent_return"
+    performance = "Draw Down: drawdown", "Log Return: log_return", "Percent Return: percent_return"
 
-    statistics = "Entropy: entropy","Kurtosis: kurtosis","Mean Absolute Deviation: mad","Median: median","Quantile: quantile","Skew: skew","Standard Deviation: stdev","Think or Swim Standard Deviation All: tos_stdevall","Variance: variance","Z Score: zscore"
+    statistics = "Entropy: entropy", "Kurtosis: kurtosis", "Mean Absolute Deviation: mad", "Median: median", "Quantile: quantile", "Skew: skew", "Standard Deviation: stdev", "Think or Swim Standard Deviation All: tos_stdevall", "Variance: variance", "Z Score: zscore"
 
-    trend = "Average Directional Movement Index: adx","Archer Moving Averages Trends: amat","Aroon & Aroon Oscillator: aroon","Choppiness Index: chop","Chande Kroll Stop: cksp","Decay: decay","Decreasing: decreasing","Detrended Price Oscillator: dpo","Increasing: increasing","Long Run: long_run","Parabolic Stop and Reverse: psar","Q Stick: qstick","Short Run: short_run","Trend Signals: tsignals","TTM Trend: ttm_trend","Vertical Horizontal Filter: vhf","Vortex: vortex","Cross Signals: xsignals"
+    trend = "Average Directional Movement Index: adx", "Archer Moving Averages Trends: amat", "Aroon & Aroon Oscillator: aroon", "Choppiness Index: chop", "Chande Kroll Stop: cksp", "Decay: decay", "Decreasing: decreasing", "Detrended Price Oscillator: dpo", "Increasing: increasing", "Long Run: long_run", "Parabolic Stop and Reverse: psar", "Q Stick: qstick", "Short Run: short_run", "Trend Signals: tsignals", "TTM Trend: ttm_trend", "Vertical Horizontal Filter: vhf", "Vortex: vortex", "Cross Signals: xsignals"
 
-    utility = "Above: above","Above Value: above_value","Below: below","Below Value: below_value","Cross: cross"
+    utility = "Above: above", "Above Value: above_value", "Below: below", "Below Value: below_value", "Cross: cross"
 
-    volatility = "Aberration: aberration","Acceleration Bands: accbands","Average True Range: atr","Bollinger Bands: bbands","Donchian Channel: donchian","Holt-Winter Channel: hwc","Keltner Channel: kc","Mass Index: massi","Normalized Average True Range: natr","Price Distance: pdist","Relative Volatility Index: rvi","Elder's Thermometer: thermo","True Range: true_range","Ulcer Index: ui"
+    volatility = "Aberration: aberration", "Acceleration Bands: accbands", "Average True Range: atr", "Bollinger Bands: bbands", "Donchian Channel: donchian", "Holt-Winter Channel: hwc", "Keltner Channel: kc", "Mass Index: massi", "Normalized Average True Range: natr", "Price Distance: pdist", "Relative Volatility Index: rvi", "Elder's Thermometer: thermo", "True Range: true_range", "Ulcer Index: ui"
 
-    volume ="Accumulation/Distribution Index: ad","Accumulation/Distribution Oscillator: adosc","Archer On-Balance Volume: aobv","Chaikin Money Flow: cmf","Elder's Force Index: efi","Ease of Movement: eom","Klinger Volume Oscillator: kvo","Money Flow Index: mfi","Negative Volume Index: nvi","On-Balance Volume: obv","Positive Volume Index: pvi","Price-Volume: pvol","Price Volume Rank: pvr","Price Volume Trend: pvt","Volume Profile: vp"
+    volume = "Accumulation/Distribution Index: ad", "Accumulation/Distribution Oscillator: adosc", "Archer On-Balance Volume: aobv", "Chaikin Money Flow: cmf", "Elder's Force Index: efi", "Ease of Movement: eom", "Klinger Volume Oscillator: kvo", "Money Flow Index: mfi", "Negative Volume Index: nvi", "On-Balance Volume: obv", "Positive Volume Index: pvi", "Price-Volume: pvol", "Price Volume Rank: pvr", "Price Volume Trend: pvt", "Volume Profile: vp"
 
     stock_name = st.text_input("Enter a stock ticker symbol", "RELIANCE")
     category = st.selectbox("Select a category", [
         'candles', 'cycles', 'momentum', 'overlap', 'performance', 'statistics', 'trend', 'volatility', 'volume'])
     button_clicked = st.button("Get Indicators")
-    st.write(f"These are the following indicators available in {category} category")
+    st.write(
+        f"These are the following indicators available in {category} category")
     if category == "candles":
         st.write(candles)
     elif category == "cycles":
@@ -148,7 +171,7 @@ with indicators:
         st.write(volatility)
     elif category == "volume":
         st.write(volume)
-        
+
     if button_clicked:
         df1 = pd.DataFrame()
         df1 = df1.ta.ticker(f"{stock_name}.NS", period="1y", asobject=True)
